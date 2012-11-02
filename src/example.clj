129b58e89b3d3a95268cddf99c200f331550e27f
Stupin Sergey <stupin.sergey@gmail.com>
2012-11-02 17:15:15 +0600

    clear code

--- src/example.clj
+++ src/example.clj
@@ -1,7 +1,6 @@
 (ns user
   [:require [yandex-blogs :as yb]])
 
-(println (yb/get {:text "Менделеев" :age-from 13}))
-;(println (get {:text "Менделеев" :age-from 13}))
+(def f (yb/get {:text "Менделеев" :age-from 13}))
 
 
